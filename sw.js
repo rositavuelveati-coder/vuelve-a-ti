32c93a5247c0a64cd538f7701f767123a74071db
Rosita Ávila Aravena <rosita.vuelveati@gmail.com>
2025-09-05 21:32:06 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = "vat-v1";
+const ASSETS = [
+  "./index.html",
+  "./home.html",
+  "./encontradora.html",
+  "./suavidad.html",
+  "./reconectar.html",
+  "./favicon.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
